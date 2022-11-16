7fec5166ac8018fe386906ec10e88af94f49fb08
Mohamed GUELLATI <Mohamed.GUELLATI.dz@viacesi.fr>
2022-11-16 15:47:46 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(maskedTextBox1.Text.Length > 0 && maskedTextBox1.Text.Length < 9)
+            {
+                textBox1.Text = maskedTextBox1.Text.ToUpper();
+                label2.Text = "";
+                label3.Text = "success";
+            }
+            else
+            {
+                label2.Text = "error";
+                label3.Text = "";
+                textBox1.Text = "";
+                return;
+            }
+            
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
